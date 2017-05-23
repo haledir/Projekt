fa50566a9cf56204676f9016ebcdacee9a3db4e1
Michael Möllmann <haledir@users.noreply.github.com>
2017-05-23 11:03:15 +0200

    login Funktion in der RESTApi eingefügt

--- REST_API/src/routes.php
+++ REST_API/src/routes.php
@@ -6,8 +6,3 @@ require __DIR__ . '/routes/benutzer_aufgaben.php';
 require __DIR__ . '/routes/fortschritt.php';
 require __DIR__ . '/routes/schritte.php';
 require __DIR__ . '/routes/status.php';
-
-// Retrieve todo with id 
-$app->get('/get', function () {
-	return "hello World";
-});
