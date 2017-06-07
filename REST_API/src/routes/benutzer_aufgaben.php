9c6e8f49c9b83091d82de1ea95eb2d30e53cde0d
Michael Möllmann <haledir@users.noreply.github.com>
2017-06-07 14:21:51 +0200

    RESTApi ergänzt

--- REST_API/src/routes/benutzer_aufgaben.php
+++ REST_API/src/routes/benutzer_aufgaben.php
@@ -33,6 +33,16 @@ $app->get('/benutzeraufgabe/{matnr}/{s_id}', function ($args) {
     return $this->response->withJson($erg);
 });
 
+
+// Alle von Benutzer {matnr} abfragen
+$app->get('/benutzeraufgabe/{matnr}', function ($args) {
+    $sth = $this->db->prepare("select schwierigkeit, max(a_id) from benutzer_aufgaben where matnr=:matnr and status = 3 group by schwierigkeit");
+    $sth->bindParam("matnr", $args['matnr']);
+    $sth->execute();
+    $erg = $sth->fetchAll();
+    return $this->response->withJson($erg);
+});
+
 // Aufgabe {a_id} mit dem Schwierigkeitsgrad {s_id} von Benutzer {matnr} abfragen
 $app->get('/benutzeraufgabe/{matnr}/{a_id}/{s_id}', function ($request, $response, $args) {
 	 $sth = $this->db->prepare("SELECT * FROM benutzer_aufgaben WHERE matnr=:matnr and a_id=:a_id and schwierigkeit=:s_id");
