ff47b9f1079fb60b9e929f0417e7c92492941cf9
Michael Möllmann <haledir@users.noreply.github.com>
2017-06-07 13:23:44 +0200

    Eingegebener Code wird in ein Array gesplittet und der Classenname gezogen.

--- REST_API/src/check_code.php
+++ REST_API/src/check_code.php
@@ -9,6 +9,19 @@ class CheckCode
 
     public function check_den_code($id, $java_code_benutzer, $app)
     {
+        // Write the code into an array an get trim
+        $temp = explode("\n", $java_code_benutzer );
+        $fileName = "";
+        foreach ($temp as $i=>$value) {
+            $temp[$i] = trim($temp[$i]);
+
+            // get the classname
+            if(substr($temp[$i],0,12) == "public class" ){
+                $tempName = explode(" ", $temp[$i]);
+                $fileName = $tempName[2];
+            }
+        }
+
         // Array which will be send back to the client
         // status = true    --> no Error and the compiled file is correct
         // status = false   --> an Error occured
@@ -18,7 +31,7 @@ class CheckCode
         $return = array("status"=>true, "compilerMessage"=>"", "result"=>"", "errorInLine"=>-1);
 
         // Save code in temp file
-        $javaFile = fopen("tempFile.java", "w");
+        $javaFile = fopen($fileName.".java", "w");
         fwrite($javaFile, $java_code_benutzer);
         fclose($javaFile);
 
@@ -27,16 +40,19 @@ class CheckCode
         $java = 'C:\Program Files\Java\jdk1.8.0_112\bin\java.exe';
 
         // Compile the generated java-File
-        $return["compilerMessage"] = shell_exec('"'.$javac.'" tempFile.java 2>&1');
+        $return["compilerMessage"] = shell_exec('"'.$javac.'" '.$fileName.'.java 2>&1');
 
         // execute the compiled java-File
         if(empty($return["compilerMessage"])) {
             $return["status"] = true;
-            $return["result"] = exec('"' . $java . '" tempFile 2>&1');
+            $return["result"] = exec('"' . $java . '" '.$fileName.' 2>&1');
         } else {
             $return["status"] = false;
             $return["result"] = "";
         }
+
+        unlink($fileName.".java");
+        unlink($fileName.".class");
 
 		$sth = $app->db->prepare("SELECT musterloesung FROM aufgaben WHERE id=:id");
 		$sth->bindParam("id", $id);
